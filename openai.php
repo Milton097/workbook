b154b4114c785a20697c5ee2f9be6c1f858e3a97
Milton097 <miltonmilto5@gmail.com>
2025-07-04 15:20:07 +0530

    Update openai.php

--- openai.php
+++ openai.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 $config = parse_ini_file("config.ini", true);
 $apiKey = $config['openai']['api_key'];
 
